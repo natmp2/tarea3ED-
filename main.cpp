2f41a8df9245ba3a52b4caae49338cf7fc3c27ce
natmp2 <126459403+natmp2@users.noreply.github.com>
2024-04-17 03:04:02 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,47 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <string>
 #include "Matrix.h"
 
 using namespace std;
 
+int random(int min, int max) { // lo saqué de la tarea 2
+    return min + rand() % (max - min + 1);
+}
+
 int main() {
-    Matrix<int> *matriz = new Matrix<int>(3,2);
-    for (int i = 0; i < matriz->getRows(); i++) {
-        for (int j = 0; j < matriz->getColumns(); j++) {
-            matriz->setValue(i, j, i+j);
+    setlocale(LC_CTYPE, "Spanish");
+    
+    int filas;
+    int columnas;
+    
+    while (true) {
+        cout << "por favor ingrese la cantidad de filas para la matriz: " << endl;
+        cin >> filas;
+        cout <<"por favor ingrese la cantidad de columnas para la matriz: " << endl;
+        cin >> columnas;
+        
+        if (filas <= 0 && columnas <=0){
+            cout << "las filas y columnas deben ser mayores que cero, por favor intente de nuevo." << endl;
+        }
+        else{
+            break;
         }
     }
-    matriz->setAll(7);
-    matriz->addRow(8);
-    matriz->addColumn(6);
+        
+    
+    Matrix<int> *matriz = new Matrix<int>(filas,columnas);
+    for (int i = 0; i < matriz->getRows(); i++){
+        for (int j = 0; j < matriz->getColumns(); j++){
+            matriz->setValue(i,j, 0); //carga la matriz inicial con 0
+        }
+    }
     matriz->print();
-    matriz->transpose();
-    cout << "\n" << endl;
-    matriz->print();
-    matriz->transpose();
-    cout << "\n" << endl;
-    matriz->print();
+    
+    
+    
     delete matriz;
     return 0;
 }
